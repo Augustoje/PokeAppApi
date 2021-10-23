b80eefef8cd31533f462d5fc7f22119767db3509
augustoje <augusto.jesus@outlook.com.br>
2021-10-23 04:01:11 -0300

    Adicionar arquivos de projeto.

---
+++ PokeApp/Models/Pokemoncontext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PokeApp.Models
+{
+    public class PokemonContext : DbContext
+    {
+        public PokemonContext(DbContextOptions<PokemonContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Pokemon> PokemonItems { get; set; }
+    }
+}
